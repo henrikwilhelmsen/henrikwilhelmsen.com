b09ea38fa4811f5af1effce54da574bc4ad56680
Henrik Wilhelmsen <contact@henrikwilhelmsen.com>
2023-03-30 18:10:21 +0200

    initial setup and project transfer

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# henrikwilhelmsen.com
-Source code for my tech animatin portfolio website.
+# [Henrikwilhelmsen.com](https://henrikwilhelmsen.com)
+
+![🚀 Deploy website on push](https://github.com/henrikwilhelmsen/henrikwilhelmsen.com/actions/workflows/main.yml/badge.svg)
+
+My personal portfolio [website](https://henrikwilhelmsen.com), built with [pico](https://picocss.com/)
+
+## Development
+
+Requires [sass](https://sass-lang.com/install)
+
+Run sass with --watch to keep css updated while working:
+
+```shell
+sass source/scss/main.scss source/css/main.css --watch
+```
+
+All pushes to main are automatically built and uploaded to the website ftp server.
---
+++ scripts/build.py
@@ -0,0 +1,58 @@
+"""Script to generate a distribution folder for the website"""
+from pathlib import Path
+from subprocess import check_call
+from shutil import copy2, rmtree
+
+PROJECT_DIR = Path(__file__).parent.parent
+SOURCE_DIR = PROJECT_DIR / "source"
+DIST_DIR = PROJECT_DIR / "dist"
+SOURCE_SCSS = SOURCE_DIR.joinpath("scss", "main.scss")
+DIST_CSS = DIST_DIR.joinpath("css", "main.css")
+
+# all files needed for the distribution of the website, excluding css
+DIST_FILES = [
+    "index.html",
+    "portfolio.html",
+    "resume.html",
+    "layered-peaks-haikei.svg",
+    "Resume-Henrik-Wilhelmsen.pdf",
+]
+
+
+def sass_compressed() -> None:
+    """Run sass to generate a compressed css file in the dist folder"""
+    args = [
+        "sass",
+        SOURCE_SCSS.as_posix(),
+        DIST_CSS.as_posix(),
+        "--style=compressed",
+        "--no-source-map",
+    ]
+    check_call(args=args)
+
+
+def copy_to_dist() -> None:
+    """Copy all files in the DIST_DIR list to the dist folder"""
+    source_files = [x for x in SOURCE_DIR.rglob("*") if x.name in DIST_FILES]
+
+    if DIST_DIR.exists():
+        rmtree(DIST_DIR)
+        DIST_DIR.mkdir(parents=True)
+
+    for src in source_files:
+        dst = Path(src.as_posix().replace("/source/", "/dist/"))
+
+        if not dst.parent.exists():
+            dst.parent.mkdir(parents=True)
+
+        copy2(src=src, dst=dst)
+
+
+def main() -> None:
+    """Main function"""
+    copy_to_dist()
+    sass_compressed()
+
+
+if __name__ == "__main__":
+    main()
